fdca3cd3f750d033626cc17bf7c3e584ae99e8ce
sp1deydev <duckhanhthientran@gmail.com>
2025-04-18 16:11:15 +0700

    test register test

--- src/main/java/com/gtel/srpingtutorial/model/request/RegisterRequest.java
+++ src/main/java/com/gtel/srpingtutorial/model/request/RegisterRequest.java
@@ -1,8 +1,12 @@
 package com.gtel.srpingtutorial.model.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
     private String phoneNumber;
 
