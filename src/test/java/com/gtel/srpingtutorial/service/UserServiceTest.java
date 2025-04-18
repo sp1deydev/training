8026e405ca46542df738476c7a7d3248f8a6515a
sp1deydev <duckhanhthientran@gmail.com>
2025-04-18 17:29:09 +0700

    send otp test

--- src/test/java/com/gtel/srpingtutorial/service/UserServiceTest.java
+++ src/test/java/com/gtel/srpingtutorial/service/UserServiceTest.java
@@ -56,5 +56,152 @@ public class UserServiceTest {
         );
 
         assertEquals(ERROR_CODE.INVALID_PARAMETER.getErrorCode(), exception.getCode());
+        assertEquals("phoneNumber is invalid", exception.getTitle());
     }
+    @Test
+    void registerWithoutPhoneNumber_failed() {
+        String phoneNumber = "";
+        String password = "abc";
+
+        RegisterRequest request = new RegisterRequest(phoneNumber, password);
+
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.registerUser(request)
+        );
+
+        assertEquals(ERROR_CODE.INVALID_PARAMETER.getErrorCode(), exception.getCode());
+        assertEquals("phoneNumber is invalid", exception.getTitle());
+    }
+    @Test
+    void registerWithoutPassword_failed() {
+        String phoneNumber = "0382349132";
+        String password = "";
+
+        RegisterRequest request = new RegisterRequest(phoneNumber, password);
+
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.registerUser(request)
+        );
+
+        assertEquals(ERROR_CODE.INVALID_PARAMETER.getErrorCode(), exception.getCode());
+        assertEquals("password is invalid", exception.getTitle());
+    }
+    @Test
+    void registerWithInvalidPhoneNumber_failed() {
+        String phoneNumber = "9342312345";
+        String password = "Qwertyu1@";
+
+        RegisterRequest request = new RegisterRequest(phoneNumber, password);
+
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.registerUser(request)
+        );
+
+        assertEquals(ERROR_CODE.INVALID_PARAMETER.getErrorCode(), exception.getCode());
+        assertEquals("phoneNumber is invalid", exception.getTitle());
+    }
+    @Test
+    void register_success() {
+        String phoneNumber = "0382349133";
+        String password = "Qwertyu1@";
+
+        RegisterRequest request = new RegisterRequest(phoneNumber, password);
+
+        OtpLimitEntity otpLimitEntity = new OtpLimitEntity();
+        otpLimitEntity.setPhoneNumber(phoneNumber);
+        otpLimitEntity.setDailyOtpCounter(0);
+        when(otpLimitRedisRepository.findById(phoneNumber)).thenReturn(Optional.of(otpLimitEntity));
+
+        RegisterResponse response = userService.registerUser(request);
+
+        assertNotNull(response);
+        assertNotNull(response.getTransactionId());
+    }
+    @Test
+    void resendOTPWithoutTransactionId_failed() {
+        String transactionId = "";
+
+        // WHEN + THEN
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.resendOTP(transactionId)
+        );
+
+        assertEquals("Transaction is not exists", exception.getTitle());
+    }
+    @Test
+    void resendOTPWithInvalidTransactionId_failed() {
+        String transactionId = "31fea99f-b3db-4dba-8f29-847b33994bb3";
+
+        // WHEN + THEN
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.resendOTP(transactionId)
+        );
+
+        assertEquals("Transaction is not exists", exception.getTitle());
+    }
+    @Test
+    void resendOTPBefore120s_failed() {
+        String transactionId = "026162be-509a-46b8-b50d-1d17edf4c982";
+        String phoneNumber = "0382349133";
+
+        RegisterUserEntity entity = new RegisterUserEntity();
+        entity.setTransactionId(transactionId);
+        entity.setPhoneNumber(phoneNumber);
+        entity.setOtpResendTime(System.currentTimeMillis() / 1000 + 120);
+
+        when(registerUserRedisRepository.findById(transactionId))
+                .thenReturn(Optional.of(entity));
+
+        // WHEN + THEN
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.resendOTP(transactionId)
+        );
+
+        assertEquals("Resend OTP too early. Please wait.", exception.getTitle());
+    }
+    @Test
+    void resendOtpAfter120s_success() {
+        // GIVEN
+        String transactionId = "026162be-509a-46b8-b50d-1d17edf4c982";
+
+        RegisterUserEntity entity = new RegisterUserEntity();
+        entity.setTransactionId(transactionId);
+        entity.setPhoneNumber("0982573860");
+        entity.setOtpResendTime(System.currentTimeMillis() / 1000 - 60);
+
+        when(registerUserRedisRepository.findById(transactionId))
+                .thenReturn(Optional.of(entity));
+
+        RegisterResponse response = userService.resendOTP(transactionId);
+
+        //THEN
+        assertNotNull(response);
+        assertNotNull(response.getTransactionId());
+    }
+    @Test
+    void sendOtpMoreThan5Times_failed() {
+        String transactionId = "026162be-509a-46b8-b50d-1d17edf4c982";
+
+        RegisterUserEntity entity = new RegisterUserEntity();
+        entity.setTransactionId(transactionId);
+        entity.setPhoneNumber("84982573860");
+        entity.setOtpResendTime(System.currentTimeMillis() / 1000 - 60);
+
+        OtpLimitEntity otpLimitEntity = new OtpLimitEntity();
+        otpLimitEntity.setPhoneNumber("84982573860");
+        otpLimitEntity.setDailyOtpCounter(5);
+        when(otpLimitRedisRepository.findById("84982573860")).thenReturn(Optional.of(otpLimitEntity));
+
+        when(registerUserRedisRepository.findById(transactionId))
+                .thenReturn(Optional.of(entity));
+
+        // WHEN + THEN
+        ApplicationException exception = assertThrows(ApplicationException.class, () ->
+                userService.resendOTP(transactionId)
+        );
+        assertEquals("OTP limit reached", exception.getTitle());
+    }
+
+    // CONFIRM OTP
+
 }
